2205c7c185cbbd38689fa7b48caa62f844b0ebcc
CDenisLaptop <chepigadeveloper@gmail.com>
2021-03-14 18:55:35 +0200

    Task 3

--- CodeWars/script.js
+++ CodeWars/script.js
@@ -36,3 +36,37 @@ function sumTwoSmallestNumbers(numbers) {
   return result
 }
 */
+/*
+Task 3
+
+Your task is to sort a given string. Each word in the string will contain a single number. This number is the position the word should have in the result.
+Note: Numbers can be from 1 to 9. So 1 will be the first word (not 0).
+If the input string is empty, return an empty string. The words in the input String will only contain valid consecutive numbers.
+
+Examples
+"is2 Thi1s T4est 3a"  -->  "Thi1s is2 3a T4est"
+"4of Fo1r pe6ople g3ood th5e the2"  -->  "Fo1r the2 g3ood 4of th5e pe6ople"
+""  -->  ""
+
+console.log(order("is2 Thi1s T4est 3a"));
+
+function order(words){
+  if(words === '') {
+    return words;
+  }
+  let result = [];
+  let arrWords = words.split(' ');
+  arrWords.forEach((item)=>{
+    for(let i=0; i<item.length; i++){
+      if(Number.isInteger(+item[i])){
+        result[+item[i] - 1] = item;
+      }
+    }
+    result.join(' ');
+  });
+  return result;
+}
+function order(words){
+    return words.split(' ').sort((wordA, wordB) => wordA.match(/\d+/) > wordB.match(/\d+/)).join(' ');
+  }
+*/
