1c622c6c7e5c22e86f8a2a3a1d56a89c8a0958a7
Denys Chepiha <chepigadeveloper@gmail.com>
2023-03-08 00:01:45 +0100

    Delete test

--- OOP/index.js
+++ OOP/index.js
@@ -25,4 +25,3 @@ console.log(' user1', user1);
 console.log( 'user1.__proto__ ', user1.__proto__);
 console.log(' user1.__proto__ === User.prototype ',user1.__proto__ === User.prototype);
 console.log(' User is prototype of object user1? ',User.prototype.isPrototypeOf(user1));
-const a = 'test'
